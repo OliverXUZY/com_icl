f14dae5ccbb2227b43ccca20764ed97d89812a55
Zhuoyan Xu <zxu68341@icloud.com>
2024-05-25 11:16:13 -0500

    update script

--- run_logic.sh
+++ run_logic.sh
@@ -3,9 +3,9 @@
 #tasks=names2_upper,plusOne,plusOne_upper,plusOne_upper_compose_incontext
 # tasks=a_level,b_level,ab_level,ab_level_compose_incontext
 # tasks=a_level_symbol
-tasks=a_level_symbol,ab_level_symbol,ab_level_compose_incontext_symbol
+#tasks=a_level_symbol,ab_level_symbol,ab_level_compose_incontext_symbol
 declare -a num_fewshots=(10)
-
+tasks=reverse,twoSum,reverse_twoSum,ciphar,cipher_twoSum,reverse_twoSum_compose_incontext,cipher_twoSum_compose_incontext
 #dir=upper_plusOne
 #declare -a models=("EleutherAI/pythia-2.8b" "EleutherAI/pythia-6.9b" "EleutherAI/pythia-12b" "huggyllama/llama-7b" "huggyllama/llama-13b" "huggyllama/llama-30b")
 #declare -a models=("EleutherAI/pythia-2.8b" "EleutherAI/pythia-6.9b" "EleutherAI/pythia-12b" "meta-llama/Llama-2-7b-hf" "meta-llama/Llama-2-13b-hf")
@@ -18,8 +18,9 @@ declare -a num_fewshots=(10)
 #declare -a models=("google/gemma-2b-it" "google/gemma-7b-it") 
 # declare -a models=("meta-llama/Llama-2-7b-hf" "mistralai/Mistral-7B-v0.1")
 # declare -a models=("mistralai/Mistral-7B-v0.1")
-declare -a models=("meta-llama/Llama-2-7b-hf" "mistralai/Mistral-7B-Instruct-v0.2")
-# declare -a models=("meta-llama/Llama-2-13b-hf"  "mistralai/Mixtral-8x7B-Instruct-v0.1" "meta-llama/Llama-2-70b-hf")
+#declare -a models=("meta-llama/Llama-2-7b-hf" "mistralai/Mistral-7B-Instruct-v0.2")
+declare -a models=("meta-llama/Llama-2-7b-hf")
+#declare -a models=("meta-llama/Llama-2-13b-hf"  "mistralai/Mixtral-8x7B-Instruct-v0.1" "meta-llama/Llama-2-70b-hf")
 # declare -a models=("mistralai/Mistral-7B-v0.1" "mistralai/Mixtral-8x7B-v0.1")
 #declare -a models=("allenai/OLMo-1B" "allenai/OLMo-7B-Instruct")
 
@@ -30,7 +31,8 @@ for seed in "${seeds[@]}"; do
     #dir=equation/mod_twoSum
     #dir=upper_plusOne/"seed${seed}"
     #dir=swap/no_instruction
-    dir=hierarchy/instruction   
+    #dir=hierarchy/instruction  
+    dir=twoSum_reverse_ciphar
     for model in "${models[@]}"; do
         model_filename=$(echo "$model" | tr '/' '_')
         echo "Model: $model"
